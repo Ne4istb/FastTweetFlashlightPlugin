80ab71c3decf9f6fe97deaff7ad1eed9f8e9fa5e
Андрей <anechytailov@gmail.com>
2015-04-05 02:28:55 +0300

    Implemented angular search plugin

--- angulardocs.bundle/plugin.py
+++ angulardocs.bundle/plugin.py
@@ -6,33 +6,26 @@ import i18n
 def results(parsed, original_query):
 
     angular_specs = [
-        ["~query", "https://docs.angularjs.org/api"]
+        ["~query", "http://docs-angular.herokuapp.com/#/<query>"]
     ]
     for key, url in angular_specs:
         if key in parsed:
             search_query = parsed[key].encode('UTF-8')
-            title = i18n.localstr("Search in Angular Docs '{0}'").format(search_query)
+            title = i18n.localstr("Search in Angular Docs for '{0}'").format(search_query)
+            search_url = url.replace("<query>", urllib.quote(search_query))
             return {
                 "title": title,
-                "run_args": [url],
+                "run_args": [search_url],
                 "html": """
                 <script>
                 setTimeout(function() {
-                    window.location = %s;
-                    setTimeout(function() {
-                        alert('directive');
-                        var searchInput = document.getElementsByClassName('search-query');
-                        var scope = angular.element(searchInput).scope();
-                        scope.q = 'directive';
-                        scope.search = 'directive';
-                    }, 1000);
+                    window.location = %s
                 }, 500);
                 </script>
-                """ % (json.dumps(url)),
+                """ % (json.dumps(search_url)),
                 "webview_user_agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53",
                 "webview_links_open_in_browser": True
             }
-
 
 def run(url):
     import os
